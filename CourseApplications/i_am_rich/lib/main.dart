1a4de02ad8e1c69c98958fa10e707f5a5f29b263
Mohammad Khan <mohammad.buet@gmail.com>
2022-12-19 21:46:33 +0900

    network image added

--- CourseApplications/i_am_rich/lib/main.dart
+++ CourseApplications/i_am_rich/lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey ,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text('I Am Rich'),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'),
+          ),
+        ),
       ),
     ),
   );
