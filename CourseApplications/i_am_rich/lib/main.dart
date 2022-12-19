754874c5960f2124f9fcd967c7023a9e7817b1db
Mohammad Khan <mohammad.buet@gmail.com>
2022-12-19 21:58:34 +0900

    asset image added

--- CourseApplications/i_am_rich/lib/main.dart
+++ CourseApplications/i_am_rich/lib/main.dart
@@ -12,7 +12,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage('https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
