432029ce4c7a58b0474868234b676174124b590b
Mohammad Khan <mohammad.buet@gmail.com>
2022-12-23 16:23:21 +0900

    challenge 1 done

--- CourseApplications/dicee-flutter/lib/main.dart
+++ CourseApplications/dicee-flutter/lib/main.dart
@@ -25,6 +25,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -46,9 +47,13 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Right button got pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                  print('Right button got pressed $rightDiceNumber');
+                });
+
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
