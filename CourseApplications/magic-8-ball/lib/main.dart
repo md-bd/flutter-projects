dc1992adc818e2553dda9d93419a12f728e75700
Mohammad Khan <mohammad.buet@gmail.com>
2022-12-23 17:16:02 +0900

    step 5 randomise it done

--- CourseApplications/magic-8-ball/lib/main.dart
+++ CourseApplications/magic-8-ball/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
   MaterialApp(
@@ -29,12 +30,15 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+
+  int ballNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextButton(
         onPressed: () {
-          print('I got clicked');
+          ballNumber = Random().nextInt(5);
+          print(ballNumber);
         },
         child: Image.asset('images/ball1.png'),
       ),
