89bc746204d18de55a3f8aa197d009c60faa260b
Mohammad Khan <mohammad.buet@gmail.com>
2022-12-23 17:05:52 +0900

    statefull widget added

--- CourseApplications/magic-8-ball/lib/main.dart
+++ CourseApplications/magic-8-ball/lib/main.dart
@@ -15,7 +15,22 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
         title: Text('Ask Me Anything'),
       ),
-      body: Container(),
+      body: Ball(),
     );
+  }
+}
+
+
+class Ball extends StatefulWidget {
+  //const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
