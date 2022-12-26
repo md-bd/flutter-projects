c303196b0552da5ff262ea30d7c1b438189edaf7
Mohammad Khan <mohammad.buet@gmail.com>
2022-12-26 16:05:48 +0900

    play multiple sound done - had to change code because of new package version

--- CourseApplications/xylophone/lib/main.dart
+++ CourseApplications/xylophone/lib/main.dart
@@ -1,14 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+//
+// void main() => runApp(XylophoneApp());
+//
+// class XylophoneApp extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: SafeArea(
+//           child: Center(
+//             child: TextButton(
+//               onPressed: () {
+//
+//                 final audioCache = AudioCache();
+//                 AudioPlayer audioPlayer = AudioPlayer();
+//                 await audioPlayer.play(DeviceFileSource('note1.wav'));
+//
+//               },
+//               child: Text('Click me'),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+void main() => runApp(xylophoneapp());
+
+class xylophoneapp extends StatefulWidget {
+  // const xylophoneapp({Key? key}) : super(key: key);
+
+  @override
+  State<xylophoneapp> createState() => _xylophoneappState();
+}
+
+class _xylophoneappState extends State<xylophoneapp> {
+
+  void playSound(int soundNumber) {
+    AudioPlayer audioPlayer = AudioPlayer();
+    audioPlayer.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                  ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
+                  ),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                  ),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                  ),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.teal),
+                  ),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  ),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.purple),
+                  ),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
