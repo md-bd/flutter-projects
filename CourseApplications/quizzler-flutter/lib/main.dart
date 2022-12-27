9f5c4d989f9454975d95e10c3d2b899f78284244
Mohammad Khan <mohammad.buet@gmail.com>
2022-12-27 11:04:10 +0900

    displaying the questions added

--- CourseApplications/quizzler-flutter/lib/main.dart
+++ CourseApplications/quizzler-flutter/lib/main.dart
@@ -26,10 +26,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
-  List<Icon> scorekeeper = [
-    Icon(Icons.check, color: Colors.green,),
-    Icon(Icons.close, color: Colors.red),
+  List<Icon> scorekeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,8 +77,10 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  scorekeeper.add(Icon(Icons.check, color: Colors.green,),);
+                  questionNumber++;
+                  // scorekeeper.add(Icon(Icons.check, color: Colors.green,),);
                 });
+                print(questionNumber);
 
               },
             ),
@@ -96,7 +102,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                //The user picked true.
+                setState(() {
+                  questionNumber++;
+                  // scorekeeper.add(Icon(Icons.check, color: Colors.green,),);
+                });
+                print(questionNumber);
               },
             ),
           ),
