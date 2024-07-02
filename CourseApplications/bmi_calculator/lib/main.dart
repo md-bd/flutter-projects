92509fd12940abe8f6bff6f7e9a4e6dc5e5878bc
Mohammad Khan <mohammad.buet@gmail.com>
2024-07-02 03:01:44 -0400

    How to use flutter themes - done

--- CourseApplications/bmi_calculator/lib/main.dart
+++ CourseApplications/bmi_calculator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,36 +7,19 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: const Color(0xFF0A0E21),
         scaffoldBackgroundColor: const Color(0xFF0A0E21),
       ),
+      // theme: ThemeData(
+      //   // primaryColor: const Color(0xFF0A0E21),
+      //   // appBarTheme: AppBarTheme(
+      //   //   color: Color(0xFF0AFFFF),
+      //   // ),
+      //   primaryColor: const Color(0xFF0A0E21),
+      //   scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      // ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-
-        },
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
