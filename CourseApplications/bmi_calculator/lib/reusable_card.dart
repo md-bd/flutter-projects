dd8d9b26929e60eaee92b59cb7dc7d840c434dc3
Mohammad Khan <mohammad.buet@gmail.com>
2024-07-03 00:29:44 -0400

    Functions as first order objects done

--- CourseApplications/bmi_calculator/lib/reusable_card.dart
+++ CourseApplications/bmi_calculator/lib/reusable_card.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, this.cardChild});
+  ReusableCard({required this.colour, this.cardChild, this.onPress});
 
   final Color colour;
   final Widget? cardChild;
+  final Function()? onPress; // https://stackoverflow.com/questions/65374364/passing-a-function-as-parameter-in-flutter-with-null-safety
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: colour,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
+        height: 200.0,
+        width: 170.0,
+        child: cardChild,
       ),
-      height: 200.0,
-      width: 170.0,
-      child: cardChild,
     );
   }
 }
