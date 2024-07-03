4587dfc680dade58095d2a3f9e5cd92aad608fad
Mohammad Khan <mohammad.buet@gmail.com>
2024-07-03 19:12:02 -0400

    asynchronous delay demo

--- CourseApplications/clima/lib/scratch.dart
+++ CourseApplications/clima/lib/scratch.dart
@@ -17,9 +17,11 @@ void task1() {
 
 void task2()  {
   Duration threeSeconds = Duration(seconds: 3);
-  sleep(threeSeconds);
-  String result = 'task 2 data';
-  print('Task 2 complete');
+
+  Future.delayed(threeSeconds, (){
+    String result = 'task 2 data';
+    print('Task 2 complete');
+  });
 }
 
 void task3() {
