ba1a248fcf215f735b62135dff0fac27b1428e2f
Mohammad Khan <mohammad.buet@gmail.com>
2024-07-04 02:11:39 -0400

    getting actual weather data from the open weather map api done

--- CourseApplications/clima/lib/screens/loading_screen.dart
+++ CourseApplications/clima/lib/screens/loading_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+
+const apiKey = '5643b8f90e9325a704230515c2dfadeb';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,21 +13,33 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double? latitude;
+  double? longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
 
     Location location = Location();
     await location.getCurrentLocation();
 
-    print(location.latitude);
-    print(location.longitude);
-  }
+    latitude = location.latitude;
+    longitude = location.longitude;
+    // print(latitude);
+    // print(longitude);
 
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
+    double temperature = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String cityName = weatherData['name'];
+  }
 
 
   @override
