66374f73de8ee6dc81fab958097d765bc1071b34
Mohammad Khan <mohammad.buet@gmail.com>
2024-07-07 18:12:42 -0400

    Level 3 - Gold Medal done

--- CourseApplications/bitcoin_ticker/lib/price_screen.dart
+++ CourseApplications/bitcoin_ticker/lib/price_screen.dart
@@ -79,6 +79,22 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   //TODO: For bonus points, create a method that loops through the cryptoList and generates a CryptoCard for each.
+   Column makeCards() {
+   List<CryptoCard> cryptoCards = [];
+   for (String crypto in cryptoList) {
+     cryptoCards.add(
+       CryptoCard(
+         cryptoCurrency: crypto,
+         selectedCurrency: selectedCurrency,
+         value: isWaiting ? '?' : coinValues[crypto] ?? '',
+       ),
+     );
+   }
+   return Column(
+     crossAxisAlignment: CrossAxisAlignment.stretch,
+     children: cryptoCards,
+   );
+ }
 
   @override
   Widget build(BuildContext context) {
@@ -93,14 +109,7 @@ class _PriceScreenState extends State<PriceScreen> {
           //TODO 1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
           //TODO 2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
           //TODO 3: You'll need to use a Column Widget to contain the three CryptoCards.
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              CryptoCard(value: isWaiting ? '?' : coinValues['BTC'] ?? '*', selectedCurrency: selectedCurrency, cryptoCurrency: 'BTC',),
-              CryptoCard(value: isWaiting ? '?' : coinValues['ETH'] ?? '*', selectedCurrency: selectedCurrency, cryptoCurrency: 'ETH',),
-              CryptoCard(value: isWaiting ? '?' : coinValues['LTC'] ?? '*', selectedCurrency: selectedCurrency, cryptoCurrency: 'LTC',),
-            ],
-          ),
+          makeCards(),
 
           Container(
             height: 150.0,
